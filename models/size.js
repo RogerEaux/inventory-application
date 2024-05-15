c4b3bbeb884882d653475f182460965a1875a16b
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-14 20:37:36 -0600

    Add size schema and model

---
+++ models/size.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const SizeSchema = new mongoose.Schema({
+  name: { type: String, required: true, minLength: 3 },
+  description: { type: String, required: true, maxLength: 300 },
+});
+
+SizeSchema.virtual('url').get(function () {
+  return `/inventory/size/${this.name}`;
+});
+
+const Size = mongoose.model('Size', SizeSchema);
+
+export default Size;
