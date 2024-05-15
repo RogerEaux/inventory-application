757e16c9e847373bdc676ad5d00e3cd35d14d077
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-14 23:29:03 -0600

    Add inventory router to handle all inventory routes

---
+++ routes/inventory.js
@@ -0,0 +1,60 @@
+import { Router } from 'express';
+import asyncHandler from 'express-async-handler';
+import * as sizeController from '../controllers/sizeController.js';
+import * as breedController from '../controllers/breedController.js';
+import * as dogController from '../controllers/dogController.js';
+
+const router = Router();
+
+// Index Route
+
+router.get(
+  '/',
+  asyncHandler((req, res, next) => {
+    res.send('We get to it when we get to it! - Index');
+  })
+);
+
+// Size Routes
+
+router.get('/size/create', sizeController.sizeCreateGet);
+router.post('/size/create', sizeController.sizeCreatePost);
+
+router.get('/size/:id/delete', sizeController.sizeDeleteGet);
+router.post('/size/:id/delete', sizeController.sizeDeletePost);
+
+router.get('/size/:id/update', sizeController.sizeUpdateGet);
+router.post('/size/:id/update', sizeController.sizeUpdatePost);
+
+router.get('/size/:id', sizeController.sizeDetail);
+router.get('/sizes', sizeController.sizeList);
+
+// Breed Routes
+
+router.get('/breed/create', breedController.breedCreateGet);
+router.post('/breed/create', breedController.breedCreatePost);
+
+router.get('/breed/:id/delete', breedController.breedDeleteGet);
+router.post('/breed/:id/delete', breedController.breedDeletePost);
+
+router.get('/breed/:id/update', breedController.breedUpdateGet);
+router.post('/breed/:id/update', breedController.breedUpdatePost);
+
+router.get('/breed/:id', breedController.breedDetail);
+router.get('/breeds', breedController.breedList);
+
+// Dog Routes
+
+router.get('/dog/create', dogController.dogCreateGet);
+router.post('/dog/create', dogController.dogCreatePost);
+
+router.get('/dog/:id/delete', dogController.dogDeleteGet);
+router.post('/dog/:id/delete', dogController.dogDeletePost);
+
+router.get('/dog/:id/update', dogController.dogUpdateGet);
+router.post('/dog/:id/update', dogController.dogUpdatePost);
+
+router.get('/dog/:id', dogController.dogDetail);
+router.get('/dogs', dogController.dogList);
+
+export default router;
