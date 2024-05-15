e93ce47d27378d600819b4b2f35f65d737f64a02
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-14 22:57:25 -0600

    Change description maxLength to 500

--- models/breed.js
+++ models/breed.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 
 const BreedSchema = new mongoose.Schema({
   name: { type: String, required: true, maxLength: 100 },
-  description: { type: String, required: true, maxLength: 300 },
+  description: { type: String, required: true, maxLength: 500 },
   size: { type: mongoose.Schema.Types.ObjectId, ref: 'Size', required: true },
   life_expectancy: { type: Number, required: true },
 });
