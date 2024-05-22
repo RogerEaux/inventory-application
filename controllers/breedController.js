89b7a2656a398d25347f2fc8a0331519f1d5370e
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-21 18:00:53 -0600

    Sort breed list query by name

--- controllers/breedController.js
+++ controllers/breedController.js
@@ -2,7 +2,7 @@ import Breed from '../models/breed.js';
 import asyncHandler from 'express-async-handler';
 
 export const breedList = asyncHandler(async (req, res, next) => {
-  const breedList = await Breed.find({}, 'name').exec();
+  const breedList = await Breed.find({}, 'name').sort({ name: 1 }).exec();
 
   res.render('breed/breedList', { breedList });
 });
