e735b6ffed65a739717bd1933d19ddf72eb56b47
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-22 00:26:33 -0600

    Add dog update POST and GET controllers

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -116,9 +116,85 @@ export const dogDeletePost = asyncHandler(async (req, res, next) => {
 });
 
 export const dogUpdateGet = asyncHandler(async (req, res, next) => {
-  res.send('We get to it when we get to it! - Dog Update Get');
+  const [dog, breeds] = await Promise.all([
+    Dog.findById(req.params.id).exec(),
+    Breed.find({}, 'name').sort({ name: 1 }).exec(),
+  ]);
+
+  if (!dog) {
+    const err = new Error('Dog not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('dog/dogForm', { title: 'Update Dog', breeds, dog, errors: null });
 });
 
-export const dogUpdatePost = asyncHandler(async (req, res, next) => {
-  res.send('We get to it when we get to it! - Dog Update Post');
-});
+export const dogUpdatePost = [
+  body('name')
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage('Name must not be empty')
+    .isLength({ max: 100 })
+    .withMessage('Name must be less than 100 characters'),
+
+  body('breed', 'Breed must not be empty').trim().notEmpty().escape(),
+
+  body('age')
+    .optional({ values: 'falsy' })
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage('Age must not be empty')
+    .isNumeric({ min: 0 })
+    .withMessage('Age must be a number greater than 0'),
+
+  body('weight')
+    .optional({ values: 'falsy' })
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage('Weight must not be empty')
+    .isNumeric({ min: 0 })
+    .withMessage('Weight must be a number greater than 0'),
+
+  body('height')
+    .optional({ values: 'falsy' })
+    .trim()
+    .notEmpty()
+    .escape()
+    .withMessage('Height must not be empty')
+    .isNumeric({ min: 0 })
+    .withMessage('Height must be a number greater than 0'),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const newDog = {
+      name: req.body.name,
+      breed: req.body.breed,
+      _id: req.params.id,
+    };
+
+    req.body.age ? (newDog.age = req.body.age) : null;
+    req.body.weight ? (newDog.weight = req.body.weight) : null;
+    req.body.height ? (newDog.height = req.body.height) : null;
+
+    const dog = new Dog(newDog);
+
+    if (!errors.isEmpty()) {
+      const breeds = await Breed.find({}, 'name').sort({ name: 1 }).exec();
+
+      res.render('dog/dogForm', {
+        title: 'Create Dog',
+        breeds,
+        dog,
+        errors: errors.array(),
+      });
+    } else {
+      await Dog.findByIdAndUpdate(req.params.id, dog);
+      res.redirect(dog.url);
+    }
+  }),
+];
