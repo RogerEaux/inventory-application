c700f7b03e95b5474f06a5dc2285d363428624a0
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-21 18:15:03 -0600

    Fix dog Schema breed required typo

--- models/dog.js
+++ models/dog.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const DogSchema = new mongoose.Schema({
   name: { type: String, required: true, maxLength: 100 },
-  breed: { type: mongoose.Schema.Types.ObjectId, ref: 'Breed', require: true },
+  breed: { type: mongoose.Schema.Types.ObjectId, ref: 'Breed', required: true },
   age: { type: Number },
   weigth: { type: Number },
   height: { type: Number },
