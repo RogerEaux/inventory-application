e99ba53538a595262263134d19f7401c2e3d28f8
RogerEaux <121988408+RogerEaux@users.noreply.github.com>
2024-05-25 20:50:17 -0600

    Fix multer destination path by prefixing ./

--- utils/multer.js
+++ utils/multer.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, process.cwd() + '/public/uploads');
+    cb(null, './public/uploads/');
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
